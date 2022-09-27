a6a3ed8826eb3c0f918a5853a5085bdeaad4d290
sergo-code <s.ivanoff-71@yandex.ru>
2022-09-27 16:30:49 +0300

    Added time zone

--- services/power_switch_message.py
+++ services/power_switch_message.py
@@ -5,16 +5,21 @@ import os
 
 from .auto_raise_resume import tasks
 from .connecting import bot
+from .env import Config
 
 
 async def on_startup(dispatcher: Dispatcher) -> None:
     asyncio.create_task(tasks())
+    os.environ['TZ'] = Config.time_zone
+    time.tzset()
     text = '🟩 Бот включился' \
            f'\n{time.strftime("%H:%M:%S")}'
     await bot.send_message(os.getenv('admin_tg'), text)
 
 
 async def on_shutdown(dispatcher: Dispatcher) -> None:
+    os.environ['TZ'] = Config.time_zone
+    time.tzset()
     text = '🟥 Бот выключился' \
            f'\n{time.strftime("%H:%M:%S")}'
     await bot.send_message(os.getenv('admin_tg'), text)
